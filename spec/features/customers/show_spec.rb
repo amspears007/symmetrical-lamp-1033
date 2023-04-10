775216f9667b64e9360832b3393d3ca9da1e3a44
Amy Spears <amspears007@gmail.com>
2023-04-10 10:29:02 -0600

    wrote tests for customer show page and updated data in let block

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'employees show page' do
+RSpec.describe 'customer show page', type: :feature do
 
   let!(:king_soopers) {Supermarket.create!(name:'King Soopers', location:'111 St')}
   let!(:safeway) {Supermarket.create!(name:'Safeway', location:'222 St')}
   let!(:costco) {Supermarket.create!(name:'Costco', location:'333 St')}
 
-  let!(:apple) {kingsoopers.items.create!(name:'granny smith', price: 1)}
-  let!(:chicken) {kingsoopers.items.create!(name:'chicken', price: 6)}
-  let!(:cake) {kingsoopers.items.create!(name:'choc cake', price: 3)}
-  let!(:candy) {kingsoopers.items.create!(name:'pez', price: 2)}
+  let!(:apple) {king_soopers.items.create!(name:'granny smith', price: 1)}
+  let!(:chicken) {king_soopers.items.create!(name:'chicken', price: 6)}
+  let!(:cake) {king_soopers.items.create!(name:'choc cake', price: 3)}
+  let!(:candy) {king_soopers.items.create!(name:'pez', price: 2)}
   let!(:pepper) {safeway.items.create!(name:'bell pepper', price: 2)}
   let!(:cookies) {safeway.items.create!(name:'oreos', price: 4)}
 
@@ -17,9 +17,25 @@ RSpec.describe 'employees show page' do
   let!(:elaine) {Customer.create!(name:'Elaine')}
   let!(:george) {Customer.create!(name:'George')}
 
-  let!(:jerry_chicken) CustomerItem.create!(customer: jerry.id, item: chicken.id)
-  let!(:jerry_apple) CustomerItem.create!(customer: jerry.id, item: apple.id)
-  let!(:elaine_candy) CustomerItem.create!(customer: elaine.id, item: candy.id)
+  let!(:jerry_chicken) {CustomerItem.create!(customer_id: jerry.id, item_id: chicken.id)}
+  let!(:jerry_apple) {CustomerItem.create!(customer_id: jerry.id, item_id: apple.id)}
+  let!(:jerry_pepper) {CustomerItem.create!(customer_id: jerry.id, item_id: pepper.id)}
+  let!(:elaine_candy) {CustomerItem.create!(customer_id: elaine.id, item_id: candy.id)}
+
+describe "User Story 1 When I visit a customer show page" do
+  it "I see the customer's name and I see its a list of its items with item's name, price, and the name of the supermarket that it belongs to" do
+  visit "/customers/#{jerry.id}"
+    
+    expect(page).to have_content('Name: Jerry')
+    expect(page).to have_content('Item: chicken')
+    expect(page).to have_content('Price: 6')
+
+    expect(page).to have_content('Supermarket: King Soopers')
+    expect(page).to_not have_content('Name: George')
+    expect(page).to_not have_content('Item: oreos')
+  end
+  end
+end
 
   
 
