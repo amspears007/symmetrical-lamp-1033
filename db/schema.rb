250cbf9a31c7274889d52630a400e2f96cacfc6a
Amy Spears <amspears007@gmail.com>
2023-04-10 09:23:30 -0600

    Add customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_140409) do
+ActiveRecord::Schema.define(version: 2023_04_10_152135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
